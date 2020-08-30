b2ae0aa2f036e7fbb50e725f6a7a19ca283e8b50
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 10:58:50 -0400

    return true if even or false otherwise

---
+++ isEven.ts
@@ -0,0 +1,8 @@
+// JOSE C.S. CURET
+// takes a positive int and return true if even or false otherwise
+
+function isEven (n: number){
+    return (n % 2 == 0) ? true : false;
+}
+
+console.log(isEven(20));
