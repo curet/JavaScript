55ae5d5143390eb0ffcdfe242b62817e3da03026
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 10:30:28 -0400

    program display the fav Lord of the Rings

---
+++ favLORD.ts
@@ -0,0 +1,27 @@
+// JOSE C.S. CURET
+// This program will display favorite The Lord of The Ring movie of the 3
+// from a random group of imaginary people
+// displaying 0's on the front to have a balance on how the display looks like
+
+function favLORD (first: number, second: number, third: number){
+
+    let firstString = String(first);
+    while(firstString.length < 5){
+        firstString = "0" + firstString;
+    }
+    console.log(firstString);
+
+    let secondString = String(first);
+    while(secondString.length < 5){
+        secondString = "0" + secondString;
+    }
+    console.log(secondString);
+
+    let thirdString = String(third);
+    while(thirdString.length < 5){
+        thirdString = "0" + thirdString;
+    }
+    console.log(thirdString);
+}
+
+favLORD(20,10,12);
