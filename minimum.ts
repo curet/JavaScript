06f92071cb244102c714cb0352c2957534ec0ad2
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 10:53:14 -0400

    func takes two arguments and returns the minimum

---
+++ minimum.ts
@@ -0,0 +1,8 @@
+// JOSE C.S. CURET
+// takes the min of two arguments and returns their minimum.
+
+function minimum (x: number, y:number){
+    return (x > y) ? y : x ; 
+}
+
+console.log(minimum(-1, -5));
