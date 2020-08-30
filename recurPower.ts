8f51fa30d3566c4682ef88ba27903f048d3ebddf
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 09:57:37 -0400

    power function recursively

---
+++ recurPower.ts
@@ -0,0 +1,10 @@
+// JOSE C.S. CURET
+// A recursive power function
+
+function power(base: number, exponent: number){
+    if (exponent == 0) return 1;
+    else if (exponent == 1) return base;
+    else return base * power(base, exponent - 1);
+}
+
+console.log(power(2,7));
