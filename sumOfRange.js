292202524938f0ca45a52b590570780bde949aae
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 21:51:13 -0400

    added `step` feature to the range function
     `step` indicate the increment behavior

--- sumOfRange.js
+++ sumOfRange.js
@@ -4,18 +4,18 @@
 // Write a range function that takes two arguments, start and end,
 // and returns an array containing all the numbers from start up to (and including) end.
 
-function range(n, m){
+function range(n, m, step = 2){
     
     let arre = [];
 
-    for(let i = n; i <= m; i++){
+    for(let i = n; i <= m; i+= step){
         arre.push(i);
     }
 
     return arre;
 }
 
-console.log(range(1,10));
+console.log(range(-1,5));
 
 // [2].
 // Next, write a sum function that takes an array of numbers and
