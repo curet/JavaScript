9af6fd04fd80a0999b0564a9391b172cb7c178e0
José C.S. Curet <jcscuret@gmail.com>
2020-08-30 21:45:36 -0400

    two func range and sumArre
    range : create an array given initial and end point (included too)
    sumArre: sum the created array or other given array

---
+++ sumOfRange.js
@@ -0,0 +1,34 @@
+// JOSE C.S. CURET
+
+// [1].
+// Write a range function that takes two arguments, start and end,
+// and returns an array containing all the numbers from start up to (and including) end.
+
+function range(n, m){
+    
+    let arre = [];
+
+    for(let i = n; i <= m; i++){
+        arre.push(i);
+    }
+
+    return arre;
+}
+
+console.log(range(1,10));
+
+// [2].
+// Next, write a sum function that takes an array of numbers and
+// returns the sum of these numbers.
+
+function sumArre(arre){
+    let result = 0;
+
+    for(let a of arre){
+        result += a;
+    }
+
+    return result;
+}
+
+console.log(sumArre(range(1,10)));
